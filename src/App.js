85f7acc1e8ca38c69a6b42b0aab2414d47d417e1
Abhimanyu <abhimanyu.ext@telekon-digital.com>
2020-05-11 03:31:55 +0530

    Adding Code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  state = {
+    items:[],
+    id:0,
+    title:'',
+    editItem:false
+  };
+  
+  handleChange = (e) => {
+    this.setState({
+      title:e.target.value
+    })
+  };
+
+  clearList = (e) => {
+    this.setState({
+      items:[]
+    })
+  };
+
+  handleDelete = (id) => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items:filteredItems
+    })
+  };
+
+  handleEdit = (id) => {
+    
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    const selectedItem = this.state.items.find(item => item.id === id);
+
+    this.setState({
+      items:filteredItems,
+      title:selectedItem.title,
+      editItem: true,
+      id:id
+    });
+    
+  };
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+
+    const newItem = {
+      id: this.state.id,
+      title:this.state.title
+    };
+
+    const updatedItems = [...this.state.items, newItem];
+    this.setState({
+      items:updatedItems,
+      title:'',
+      id:this.state.id + 1,
+      editItem:false
+    })
+
+  };
+  render() { 
+    return (
+      <div className= "contrainer">
+        <div className= "row">
+        <div className= "col-10 mx-auto col-md-8 mt-4">
+          <h3 className="text-capitalize text-center">Todo list</h3> 
+          <TodoInput title={this.state.title} handleChange={this.handleChange} handleSubmit={this.handleSubmit} editItem={this.state.editItem}  />
+         <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete} handleEdit={this.handleEdit} />
+        </div>
+        </div>  
+      </div>
+      );
+  }
 }
-
+ 
 export default App;
